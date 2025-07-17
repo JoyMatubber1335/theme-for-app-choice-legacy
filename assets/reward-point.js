5f9978fc4b6139ef13d22d2b886518246067d504
Faisal235711 <faisal.ahmed@brainstation-23.com>
2025-07-17 14:41:01 +0600

    Remove console logs

--- assets/reward-point.js
+++ assets/reward-point.js
@@ -51,7 +51,7 @@ document.addEventListener("DOMContentLoaded", () => {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ points: pointsToRedeem }),
       });
-
+      console.log(redeemResponse);
       if (redeemResponse.discountCode) {
         alert("Points redeemed successfully!");
       }
